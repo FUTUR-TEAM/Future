03b8f9e905707a33f6905d08a8601b50151e5ff5
Karolina Scigocka <scigockakarolina@gmail.com>
2021-04-09 13:35:33 +0200

    Added eng

--- Future/tests/testthat/test_BMI.R
+++ Future/tests/testthat/test_BMI.R
@@ -1,35 +1,35 @@
 
-testthat::test_that(desc = "BMI dziaLa", {
+testthat::test_that(desc = "BMI works", {
 
   result <- BMI(100,2)
   expect <- 100/2^2
   testthat::expect_equal(result$BMI, expect)
 
   result <- BMI(50,2)
-  testthat::expect_equal(result$sylwetka, "niedowaga")
+  testthat::expect_equal(result$figure, "underweight")
 
   result <- BMI(50,1.5)
-  testthat::expect_equal(result$sylwetka, "optimum")
+  testthat::expect_equal(result$figure, "normal")
 
   result <- BMI(60,1.5)
-  testthat::expect_equal(result$sylwetka, "nadwaga")
+  testthat::expect_equal(result$figure, "overweight")
 
   result <- BMI(70,1.5)
-  testthat::expect_equal(result$sylwetka, "otyłosc")
+  testthat::expect_equal(result$figure, "obese")
 
 })
 
-testthat::test_that(desc = "BMI siE Ladnie wzszpuje", {
+testthat::test_that(desc = "BMI has expected error messeges", {
 
-  testthat::expect_error(BMI(100,"2"), "wzrost musi byc wartosciaa liczbowa")
-  testthat::expect_error(BMI("100", 2), "waga musi byc wartosciaa liczbowa")
+  testthat::expect_error(BMI(100,"2"), "growth must be numeric")
+  testthat::expect_error(BMI("100", 2), "weight must be numeric")
   testthat::expect_error(BMI(100, 2, 3), "'arg' must be NULL or a character vector")
 
 })
 
-testthat::test_that(desc = "sprawdza plec", {
+testthat::test_that(desc = "check sex", {
 
-  result <- BMI(50, 2, "kobieta")
-  testthat::expect_equal(result$sylwetka, "niedowaga")
+  result <- BMI(50, 2, "female")
+  testthat::expect_equal(result$figure, "underweight")
 
 })
