e10c413fd6e7e3194a964cfc99dd1c530ea9c27c
Karolina Scigocka <scigockakarolina@gmail.com>
2021-08-04 10:05:59 +0200

    added tests for pie chart

--- tests/testthat/test_interactive_pie_chart.R
+++ tests/testthat/test_interactive_pie_chart.R
@@ -5,9 +5,33 @@ testthat::test_that(desc = "interactive pie chart works!", {
   macro_percent <- macronutrients_of_meal(list_of_products, weight_of_products)
   result <- macro_pie_chart(macro_percent)
 
+  testthat::expect_equal(class(result), c("plotly", "htmlwidget"))
   testthat::expect_equal(length(result), 8)
-  testthat::expect_true(is.data.frame(macro_percent))
-  testthat::expect_equal(object = macro_percent$sum, expected = c(1.6, 0.8, 24.2))
-  testthat::expect_equal(object = macro_percent$percent, expected = c(6.02, 3.01, 90.98))
 })
+
+testthat::test_that(desc = "interactive pie chart has expected error messeges", {
+
+  list_of_products <- list("Banan", "Truskawki")
+  weight_of_products <- list(100, 100)
+  macro_percent <- macronutrients_of_meal(list_of_products, weight_of_products)
+  testthat::expect_error(macro_pie_chart(character()),
+                         "macro_percent must be data frame")
+
+  list_of_products <- list("Banan", "Truskawki")
+  weight_of_products <- list(100, 100)
+  macro_percent <- macronutrients_of_meal(list_of_products, weight_of_products)
+  macro_percent <- cbind(macro_percent, 1)
+  testthat::expect_error(macro_pie_chart(macro_percent),
+                         "macro_percent must have 3 columns: 'macro_name', 'sum', 'percent'")
+
+  list_of_products <- list("Banan", "Truskawki")
+  weight_of_products <- list(100, 100)
+  macro_percent <- macronutrients_of_meal(list_of_products, weight_of_products)
+  macro_percent <- rbind(macro_percent, 1)
+  testthat::expect_error(macro_pie_chart(macro_percent),
+                         "macro_percent must have 3 rows: 'protein', 'fat', 'carbohydrates'")
+
+
+  })
+
 
