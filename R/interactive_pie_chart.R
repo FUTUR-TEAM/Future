9f1a8bfd9a91ea5f66c238bb0b99c6b1deb090a5
Karolina Scigocka <scigockakarolina@gmail.com>
2021-08-05 10:32:41 +0200

    updated examples changed order of asserts removed unnesessery code in macro_pie_chart function

--- R/interactive_pie_chart.R
+++ R/interactive_pie_chart.R
@@ -8,26 +8,25 @@
 #' list_of_products <- list("Banan", "Truskawki")
 #' weight_of_products <- list(100, 100)
 #' macro_percent <- macronutrients_of_meal(list_of_products, weight_of_products)
+#' macro_pie_chart(macro_percent)
 #'
 #' @export
 macro_pie_chart <- function(macro_percent){
 
   assertthat::assert_that(is.data.frame(macro_percent),
                           msg = "macro_percent must be data frame")
-  assertthat::assert_that(all(macro_percent$macro_name %in% c("protein", "fat", "carbohydrates")),
-                          msg = "macro_percent must have 3 rows: 'protein', 'fat', 'carbohydrates'")
-  assertthat::assert_that(all(colnames(macro_percent) %in% c("macro_name", "sum", "percent")),
+  assertthat::assert_that(all(colnames(macro_percent) == c("macro_name", "sum", "percent")),
                           msg = "macro_percent must have 3 columns: 'macro_name', 'sum', 'percent'")
-
-data <- macro_percent[, c('macro_name', 'percent')]
+  assertthat::assert_that(all(macro_percent$macro_name == c("protein", "fat", "carbohydrates")),
+                          msg = "macro_percent must have 3 rows: 'protein', 'fat', 'carbohydrates'")
 
 colors <- c('rgb(208,223,247)', 'rgb(245,188,199)', 'rgb(161,240,174)')
 
 pie_chart <-
   plotly::plot_ly(
-    data,
-    labels = ~ macro_name,
-    values = ~ percent,
+    macro_percent,
+    labels = macro_percent$macro_name,
+    values = macro_percent$percent,
     type = 'pie',
     textposition = 'inside',
     texttemplate = ~ paste(macro_percent$macro_name, '%{percent:.2%f}'),
