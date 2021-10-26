7b3c3ce6b31af2e07bb775c80415e644a562586d
Karolina Scigocka <scigocka.karolina@gmail.com>
2021-10-26 07:32:03 +0000

    updated tests to handle new logic of generating input for ingredients

--- tests/testthat/test_runFutureApp.R
+++ tests/testthat/test_runFutureApp.R
@@ -2,10 +2,12 @@
 app <- shinytest::ShinyDriver$new(system.file("shiny", package = "Future"))
 
 app$snapshotInit("test_runFutureApp")
-Sys.sleep(1)
+app$waitForShiny()
 
-app$setValue(name = "main_module-product01", "Banan", iotype = "input")
-app$setValue(name = "main_module-weight01", 100, iotype = "input")
+app$click("main_module-add_one")
+app$waitForShiny()
+app$setValue(name = "main_module-product1", "Banan", iotype = "input")
+app$setValue(name = "main_module-weight1", 100, iotype = "input")
 app$setInputs(`main_module-click` = "click")
 
 app$snapshot()
@@ -15,30 +17,31 @@ testthat::expect_equal(
   "Kalorycznosc posilku wynosi 74.3 kcal."
 )
 
-app$setInputs(`main_module-product02` = "Mango")
-app$setInputs(`main_module-weight02` = 100)
-app$setInputs(`main_module-add02` = "click")
-app$setInputs(`main_module-product03` = "Awokado")
-app$setInputs(`main_module-weight03` = 100)
-app$setInputs(`main_module-add03` = "click")
-app$setInputs(`main_module-product04` = "Orzechy laskowe")
-app$setInputs(`main_module-weight04` = 100)
-app$setInputs(`main_module-add04` = "click")
-app$setInputs(`main_module-product05` = "Czekolada gorzka")
-app$setInputs(`main_module-weight05` = 100)
+app$setInputs(`main_module-add_one` = "click")
+app$setInputs(`main_module-product2` = "Mango")
+app$setInputs(`main_module-weight2` = 100)
+app$setInputs(`main_module-add_one` = "click")
+app$setInputs(`main_module-product3` = "Awokado")
+app$setInputs(`main_module-weight3` = 100)
+app$setInputs(`main_module-add_one` = "click")
+app$setInputs(`main_module-product4` = "Orzechy laskowe")
+app$setInputs(`main_module-weight4` = 100)
+app$setInputs(`main_module-add_one` = "click")
+app$setInputs(`main_module-product5` = "Czekolada gorzka")
+app$setInputs(`main_module-weight5` = 100)
 app$setInputs(`main_module-click` = "click")
 
 app$snapshot()
 all_values <- app$getAllValues()
 testthat::expect_equal(
   all_values$output$`main_module-kcalMeal`,
-  "Kalorycznosc posilku wynosi 1514.4 kcal."
+  "Kalorycznosc posilku wynosi 501.7 kcal."
 )
 
-app$setInputs(`main_module-remove05` = "click")
-app$setInputs(`main_module-remove04` = "click")
-app$setInputs(`main_module-remove03` = "click")
-app$setInputs(`main_module-remove02` = "click")
+app$setInputs(`main_module-remove5` = "click")
+app$setInputs(`main_module-remove4` = "click")
+app$setInputs(`main_module-remove3` = "click")
+app$setInputs(`main_module-remove2` = "click")
 app$setInputs(`main_module-click` = "click")
 
 app$snapshot()
